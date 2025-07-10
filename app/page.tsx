88dc79d45117ae6c80d3313a45181b6dcc89095c
Yousief Sameh <yousief.sameh@outlook.com>
2025-07-10 20:56:42 +0300

    TRPC and React Query (TanStack) Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { Suspense } from 'react';
+import { trpc, getQueryClient } from '@/trpc/server';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import Client from './client';
 
-const HomePage = () => {
+const HomePage = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAi.queryOptions({ text: "From Vibe Craft" }))
   return (
-    <div>
-      <Button variant={"default"}>Project Setup Done :)</Button>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+
   )
 }
 
